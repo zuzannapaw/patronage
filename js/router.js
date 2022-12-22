e5dccd3a726f921490b5fff75a3af7e6e28f34dd
zuzannapaw <zuzannapawlowska0@gmail.com>
2022-12-22 20:32:33 +0100

    Building routes, login and registration

---
+++ js/router.js
@@ -0,0 +1,32 @@
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+}
+
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/index.html",
+    "/registration": "/pages/registration.html",
+    "/login": "/pages/login.html",
+    "/user": "/pages/user.html",
+}
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
+
+
+
+
+
+
