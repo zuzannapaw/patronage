37f4d98a26b5997dc76e6332d20ad56256c59a6c
ZuzannaPaw <zuzannapaw@wp.pl>
2023-01-16 20:43:28 +0100

    Code cleanup

--- src/services/api/api-service.js
+++ src/services/api/api-service.js
@@ -1,3 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 const getData = async (url, headers) => {
     const response = await fetch(url, {
         headers: {
@@ -8,23 +9,3 @@ const getData = async (url, headers) => {
     return await response.json();
 }
 Object.prototype.patronage.setGlobalKey('api_getData', getData);
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-const postData = async (url = '', data = {}) => {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-}
-Object.prototype.patronage.setGlobalKey('api_postData', postData);
